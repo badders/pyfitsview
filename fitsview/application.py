25bc36d02e143a1884fc0ad1d69fa6469a0e7dee
Tom Badran <tom@badrunner.net>
2013-07-26 12:04:33 +0100

    Throttle image loads Better comment applcation class Fix minor bugs

--- fitsview/application.py
+++ fitsview/application.py
@@ -32,7 +32,7 @@ class FitsViewer(QtGui.QApplication):
     def hasImage(f):
         @wraps(f)
         def _hasImage(*args, **kwargs):
-            if args[0].fits.gc is not None:
+            if args[0].fits._gc is not None:
                 return f(*args, **kwargs)
             else:
                 return None
@@ -69,7 +69,7 @@ class FitsViewer(QtGui.QApplication):
         ui.cutLowerValue.valueChanged.connect(self.fits.setLowerCut)
 
         ui.actionOpen.triggered.connect(self.addFiles)
-        ui.actionAbout.triggered.connect(self.about)
+        ui.actionAbout.triggered.connect(self.about_ui.show)
         ui.actionSave.triggered.connect(self.saveImage)
         ui.actionExport.triggered.connect(self.exportImage)
         ui.actionFit_to_Window.triggered.connect(self.fits.zoomFit)
@@ -111,10 +111,18 @@ class FitsViewer(QtGui.QApplication):
 
         ui.tabifyDockWidget(ui.toolsDock, ui.displayDock)
 
+        # Create image load throttler
+        self._load_timer = QtCore.QTimer()
+        self._load_timer.setSingleShot(True)
+        self._load_timer.timeout.connect(self._setFileConcrete)
+
         ui.show()
         self.loadConfig()
 
     def updateStatus(self, x, y, value, ra_d, dec_d):
+        """
+        Fetch information and update status bar text
+        """
         try:
             coord = coordinates.ICRSCoordinates(ra=ra_d, dec=dec_d, unit=(units.degree, units.degree))
             ra_str = coord.ra.format(units.hour)
@@ -139,6 +147,10 @@ class FitsViewer(QtGui.QApplication):
         self.fits.setScale(self.ui.normalisation.itemText(index))
 
     def addFiles(self, *args, **kwargs):
+        """
+        Add list of files
+        Supply keyword argument 'files' or it will open a file open dialog
+        """
         if not 'files' in kwargs:
             files = QtGui.QFileDialog.getOpenFileNames(caption='Load Fits File', filter='*.fits')
         else:
@@ -149,22 +161,37 @@ class FitsViewer(QtGui.QApplication):
             self.ui.fileList.setCurrentIndex(self.model.index(0, 0))
 
     def setSelection(self, selection):
+        """
+        Set the file selection
+        """
         try:
             self.setFile(selection[0].indexes()[0])
         except IndexError:
             pass
 
     def next(self):
+        """
+        Select next file in list
+        """
         current = self.ui.fileList.currentIndex().row()
         if current < self.model.rowCount() - 1:
             self.ui.fileList.setCurrentIndex(self.model.index(current + 1, 0))
 
     def previous(self):
+        """
+        Select previous file in list
+        """
         current = self.ui.fileList.currentIndex().row()
         if current > 0:
             self.ui.fileList.setCurrentIndex(self.model.index(current - 1, 0))
 
-    def setFile(self, index):
+    def _setFileConcrete(self):
+        """
+        Actuallly perform a file load based on selection
+        This operation can be slow (order 1s) so use the setFile method instead which
+        throttles calls.
+        """
+        index = self._load_index
         item = self.model.itemFromIndex(index)
         self.fits.loadImage(str(item.fn))
         self.status.setText('')
@@ -173,6 +200,13 @@ class FitsViewer(QtGui.QApplication):
         self.ui.infoDateLabel.setText(str(dt.date()))
         self.ui.infoTimeLabel.setText(str(dt.time()))
 
+    def setFile(self, index):
+        """
+        Set the file from the list to display in the main widget
+        """
+        self._load_index = index
+        self._load_timer.start(400)
+
     def _getSettings(self):
         """
         Return the QSettings object for the program
@@ -180,6 +214,9 @@ class FitsViewer(QtGui.QApplication):
         return QtCore.QSettings('Fits Viewer', 'pyfitsview')
 
     def _loadSessionConcrete(self, filen):
+        """
+        Perform a session load
+        """
         f = open(filen)
         session = json.load(f)
         display = session['display']
@@ -194,6 +231,9 @@ class FitsViewer(QtGui.QApplication):
         self._addRecentFile(filen)
 
     def _addRecentFile(self, filen):
+        """
+        Add a filename to the recent files list
+        """
         try:
             self.recent_files.remove(filen)
         except ValueError:
@@ -203,19 +243,32 @@ class FitsViewer(QtGui.QApplication):
         self._updateRecentFiles()
 
     def loadSession(self):
+        """
+        Show open dialog  and load a session file
+        """
         filen = QtGui.QFileDialog.getOpenFileName(caption='Save Session')
-        self._loadSessionConcrete(filen)
+        if filen != '':
+            self._loadSessionConcrete(filen)
 
     def loadRecentSession(self):
+        """
+        Load a session from the recent file dialog
+        """
         action = self.sender()
         if action:
             self._loadSessionConcrete(str(action.data().toPyObject()))
 
     def _autoSaveSession(self):
+        """
+        Save the current session
+        """
         if self._session_file is not None:
             self._saveSessionConcrete(self._session_file)
 
     def _saveSessionConcrete(self, filen):
+        """
+        Perform a session load
+        """
         f = open(filen, 'w')
         display = {
             'lcut': self.ui.cutLowerValue.value(),
@@ -233,11 +286,18 @@ class FitsViewer(QtGui.QApplication):
         self._addRecentFile(filen)
 
     def saveSession(self):
+        """
+        Open a dialog and save the current sesion
+        """
         filen = QtGui.QFileDialog.getSaveFileName(caption='Save Session')
-        self._session_file = filen
-        self._saveSessionConcrete(filen)
+        if filen != '':
+            self._session_file = filen
+            self._saveSessionConcrete(filen)
 
     def loadConfig(self):
+        """
+        Load the configuration
+        """
         settings = self._getSettings()
         settings.beginGroup('Window')
         self.ui.restoreGeometry(settings.value('geometry', self.ui.saveGeometry()).toPyObject())
@@ -252,6 +312,9 @@ class FitsViewer(QtGui.QApplication):
         self._updateRecentFiles()
 
     def _updateRecentFiles(self):
+        """
+        Update the recent file menu based on the recent file list
+        """
         for i in range(len(self.recent_files)):
             fn = self.recent_files[i]
             name = os.path.basename(str(fn))
@@ -260,6 +323,9 @@ class FitsViewer(QtGui.QApplication):
             self.recent_file_acts[i].setVisible(True)
 
     def saveConfig(self):
+        """
+        Save the configuration
+        """
         settings = self._getSettings()
         settings.beginGroup('Window')
         settings.setValue('geometry', self.ui.saveGeometry())
@@ -268,6 +334,9 @@ class FitsViewer(QtGui.QApplication):
 
     @hasImage
     def saveImage(self):
+        """
+        Open a dialog and save the curent fits image
+        """
         filen = QtGui.QFileDialog.getSaveFileName(caption='Save Fits File')
         if filen != '':
             self.fits.saveImage(str(filen))
@@ -275,15 +344,18 @@ class FitsViewer(QtGui.QApplication):
 
     @hasImage
     def exportImage(self):
+        """
+        Open a dialog and export the current the image
+        """
         filen = QtGui.QFileDialog.getSaveFileName(caption='Export to File')
         if filen != '':
             self.fits.saveImage(str(filen), export=True)
             self.status.showMessage('Exported to {}'.format(str(filen)))
 
-    def about(self):
-        self.about_ui.show()
-
     def takeImage(self):
+        """
+        Take an image with the allsky camera
+        """
         port = str(self.ui.portChoice.currentText())
         self.progress = QtGui.QProgressDialog('Downloading Image from Camera ...', '', 0, 0)
         self.progress.setCancelButton(None)
@@ -298,5 +370,8 @@ class FitsViewer(QtGui.QApplication):
         self.status.setText('')
 
     def _takeImageProgress(self, percent):
+        """
+        Callback for updating the image taking progress
+        """
         self.progress.setValue(percent)
         QtGui.QApplication.processEvents()
