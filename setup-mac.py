5270cd08cdde2c00dd7c7512c1616b6800f4502e
Tom Badran <tom@badrunner.net>
2013-07-29 22:18:01 +0100

    Move mac setup to its own file

---
+++ setup-mac.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+APP = ['fitsview.py']
+DATA_FILES = ['ui']
+
+OPTIONS = {
+    'argv_emulation': True,
+    'packages': ['astropy', 'matplotlib', 'scipy', 'numpy'],
+    'includes': ['PyQt4', 'PyQt4.QtCore', 'PyQt4.QtGui', 'sip'],
+    'excludes': ['PyQt4.QtDesigner', 'PyQt4.QtNetwork', 'PyQt4.QtOpenGL', 'sympy',
+                 'PyQt4.QtScript', 'PyQt4.QtSql', 'PyQt4.QtWebKit', 'PyQt4.QtXml',
+                 'PyQt4.phonon', 'Tkinter', 'PyQt4.uic.port_v3', 'matplotlib.tests'],
+    'iconfile': 'ui/icon.icns',
+    'resources': ['ui/image-fits.icns'],
+    'plist': dict(
+        CFBundleDocumentTypes=[
+                dict(
+                    CFBundleTypeName='Flexible Image Transport System document',
+                    CFBundleTypeIconFile='ui/image-fits.icns',
+                    CFBundleTypeExtensions=['fits', 'fit', 'fts'],
+                    CFBundleTypeOSTypes=['FITS'],
+                    CFBundleTypeRole='Viewer'),
+        ],
+        CFBundleName='Fits Viewer',
+        CFBundleDisplayName='Fits Viewer',
+    )
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
